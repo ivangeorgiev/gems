eb7499613efa22994071932f1e032dc6b7dc5806
Ivan Georgiev <ivan.georgiev@gmail.com>
2020-08-25 18:39:03 +0200

    Removed secret

--- src/python-django-streaming/mysite/settings.py
+++ src/python-django-streaming/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^!m0=*d!(&o-sa@&w!$lw#tm0bvcz8)+(ss1@95bi!#_!dk1^p'
+SECRET_KEY = os.env.get('DJANGO_SECRET', 'this-must-be-very-secret-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
