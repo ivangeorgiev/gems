0baaaa955b5a80e87e9d7b6000d78854c231f9f2
Ivan Georgiev <ivan.georgiev@gmail.com>
2020-08-26 22:25:45 +0200

    version increase in setup.py

--- src/python-import-from-url/setup.py
+++ src/python-import-from-url/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyimporter",
-    version="0.0.1",
+    version="0.0.2",
     author="Example Author",
     author_email="author@example.com",
     description="Python import library extensions with implementation to import from url.",
